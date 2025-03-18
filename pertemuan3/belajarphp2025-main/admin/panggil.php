92fef94757985383cbacb6c87e6f2c48d36277f9
cintamelati13 <cntmlt130@gmail.com>
2025-03-18 19:18:01 +0700

    Add files via upload

---
+++ pertemuan3/belajarphp2025-main/admin/panggil.php
@@ -0,0 +1,20 @@
+<?php 
+    include_once 'top.php';
+?>
+<div id="page-content-wrapper">
+        <!-- Top navigation-->
+        <?php 
+            include_once 'menu.php';
+        ?>
+        <!-- Page content-->
+        <div class="container-fluid">
+            <h1 class="mt-4">Hasil UTS</h1>
+                
+        <?php 
+            include_once 'utspemrogramanweb/index.html';
+        ?>
+        </div>
+</div>
+<?php
+     include_once 'utspemrogramanweb/index.html';
+?>
---
+++ pertemuan3/belajarphp2025-main/form_nilai.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<?php 
+  require_once 'fungsiku.php';
+?>
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<h3>Form Nilai Mahasiswa</h3>
+<?php 
+    $ar_matkul = [
+        'DDP'=>'Dasar-Dasar Pemrograman',
+        'WEB1'=>'Pemrograman Web 1',
+        'WEB2'=>'Pemrograman Web 2',
+        'BASDAT'=>'Basis Data',
+        'UI/UX'=>'UI/UX',
+        "JARKOM"=>"Jaringan Komputer",
+        "SE"=>"Sistem Enterprise",
+    ];
+?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama Lengkap</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="nama" name="nama" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+    <div class="col-8">
+      <select id="matkul" name="matkul" class="custom-select">
+        <option value="0">-- Pilih Mata Kuliah --</option>
+<?php 
+    foreach($ar_matkul as $kode=>$matkul){
+        echo "<option value='$kode'>$matkul</option>";
+    }
+?>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_uts" class="col-4 col-form-label">Nilai UTS</label> 
+    <div class="col-8">
+      <input id="nilai_uts" name="nilai_uts" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_uas" class="col-4 col-form-label">Nilai UAS</label> 
+    <div class="col-8">
+      <input id="nilai_uas" name="nilai_uas" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_tugas" class="col-4 col-form-label">Tugas/Praktikum</label> 
+    <div class="col-8">
+      <input id="nilai_tugas" name="nilai_tugas" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+
+<hr>
+<?php 
+    $_nama = $_POST['nama'];
+    $_matkul = $_POST['matkul'];    
+    $_nilai_uts = $_POST['nilai_uts'];
+    $_nilai_uas = $_POST['nilai_uas'];
+    $_nilai_tugas = $_POST['nilai_tugas'];
+
+    if(isset($_nama))
+    
+    $_nilai_akhir = hitung_nilai($_nilai_uts, $_nilai_uas, $_nilai_tugas);
+    $_ket_lulus = kelulusan($_nilai_akhir);
+?>
+<h3>Hasil Input Data</h3>
+Nama Mahasiswa: <?=$_nama ?><br>
+Mata Kuliah: <?=$ar_matkul[$_matkul]; ?><br>       
+Nilai UTS: <?=$_nilai_uts; ?><br>
+Nilai UAS: <?=$_nilai_uas; ?><br>
+Nilai Tugas: <?=$_nilai_tugas; ?><br>
+Nilai Akhir: <?=$_nilai_akhir; ?><br>
+Hasil Kelulusan: <?=$_ket_lulus; ?><br>
+?>
+</body>
+</html>
